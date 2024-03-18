4764fdeaaab212a7b8cb8f92ec5f2577deb5f8dd
Noah Masoud <114415606+nomasoud@users.noreply.github.com>
2024-03-18 18:02:48 -0500

    updated position and color

--- src/component/ButtonPanel.js
+++ src/component/ButtonPanel.js
@@ -23,39 +23,39 @@ export default class ButtonPanel extends React.Component {
   // It's creating a grid of buttons using the <Button /> component.
 
   // Each Button component is passed a name and a clickHandler.
-  // Some buttons have an additional orange property to signify a styling choice.
+  // Some buttons have an additional  property to signify a styling choice.
   // The button with the name "0" has a wide property for styling as well.
   render() {
     return (
       <div className="component-button-panel">
         <div>
-          <Button name="AC" clickHandler={this.buttonClicked} />
-          <Button name="+/-" clickHandler={this.buttonClicked} />
-          <Button name="%" clickHandler={this.buttonClicked} />
-          <Button name="÷" clickHandler={this.buttonClicked} orange />
+          <Button name="÷" clickHandler={this.buttonClicked} grey />
+          <Button name="AC" clickHandler={this.buttonClicked} orange />
+          <Button name="+/-" clickHandler={this.buttonClicked} orange />
+          <Button name="%" clickHandler={this.buttonClicked} orange />
         </div>
         <div>
+          <Button name="x" clickHandler={this.buttonClicked} grey />
           <Button name="7" clickHandler={this.buttonClicked} />
           <Button name="8" clickHandler={this.buttonClicked} />
           <Button name="9" clickHandler={this.buttonClicked} />
-          <Button name="x" clickHandler={this.buttonClicked} orange />
         </div>
         <div>
+          <Button name="-" clickHandler={this.buttonClicked} grey />
           <Button name="4" clickHandler={this.buttonClicked} />
           <Button name="5" clickHandler={this.buttonClicked} />
           <Button name="6" clickHandler={this.buttonClicked} />
-          <Button name="-" clickHandler={this.buttonClicked} orange />
         </div>
         <div>
+          <Button name="+" clickHandler={this.buttonClicked} grey />
           <Button name="1" clickHandler={this.buttonClicked} />
           <Button name="2" clickHandler={this.buttonClicked} />
           <Button name="3" clickHandler={this.buttonClicked} />
-          <Button name="+" clickHandler={this.buttonClicked} orange />
         </div>
         <div>
-          <Button name="0" clickHandler={this.buttonClicked} wide  />
+          <Button name="=" clickHandler={this.buttonClicked} grey />
+          <Button name="0" clickHandler={this.buttonClicked} wide />
           <Button name="." clickHandler={this.buttonClicked} />
-          <Button name="=" clickHandler={this.buttonClicked} orange />
         </div>
       </div>
     );
